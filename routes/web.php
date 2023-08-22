2b2e0e389e11032967b9fb068846c579e4378fb9
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-08-23 00:44:25 +0900

    Initial Setup and Design

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('dashboard');
 
 
 require __DIR__ . '/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+
+                black: colors.black,
+                white: colors.white,
+                gray: colors.trueGray,
+                "gray-background": "#f7f8fc",
+                blue: "#328af1",
+                "blue-hover": "#2879bd",
+                yellow: "#ffc73c",
+                red: "#ec454f",
+                "red-100": "#fee2e2",
+                green: "#1aab8b",
+                "green-50": "#f0fdf4",
+                purple: "#8b60ed",
+            },
+            spacing: {
+                70: "17.5rem",
+                175: "43.75rem",
+            },
+            maxWidth: {
+                custom: "62.5rem",
+            },
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
