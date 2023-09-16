db38505eb295067d6f6bf61891a9394fc0a03a25
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-09-16 18:17:47 +0900

    Add Comments

--- resources/views/livewire/idea-comments.blade.php
+++ resources/views/livewire/idea-comments.blade.php
@@ -6,6 +6,7 @@
                 <livewire:idea-comment
                     :key="$comment->id"
                     :comment="$comment"
+                    :ideaUserId="$idea->user->id"
                 />
             @endforeach
 
